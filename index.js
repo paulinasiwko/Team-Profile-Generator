b022ffb2e4694111fb14cbbcf087dea78dfc7d9b
Paulina Siwko <pau.siwko@gmail.com>
2024-02-12 23:14:04 +0100

    retrieve employees details from prompts

--- index.js
+++ index.js
@@ -10,6 +10,122 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+async function getMangerDetails() {
+    return await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter manager name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter manager id:',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Enter manager email:',
+          },
+          {
+            type: 'input',
+            name: 'office',
+            message: 'Enter manager office number:',
+          },
+      ]);
+}
+
+async function getEngineerDetails() {
+    return await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter engineer name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter engineer id:',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Enter engineer email:',
+          },
+          {
+            type: 'input',
+            name: 'github',
+            message: 'Enter engineer github:',
+          },
+      ]);
+}
+
+async function getInternDetails() {
+    return await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter intern name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter intern id:',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Enter intern email:',
+          },
+          {
+            type: 'input',
+            name: 'school',
+            message: 'Enter intern school:',
+          },
+      ]);
+}
+
+async function getChoice() {
+    return await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'option',
+          message: 'Choose an option:',
+          choices: ['Add engineer', 'Add intern', 'Generate team page'],
+        },
+      ]);
+}
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+async function main() {
+    let employees = []
+    var { name, id, email, office } = await getMangerDetails();
+    const manager = new Manager(name, id, email, office)
+    employees.push(manager)
 
+    let choice = await getChoice()
+
+    while (choice.option != 'Generate team page') {
+        switch (choice.option) {
+            case 'Add engineer':
+              var { name, id, email, github } = await getEngineerDetails();
+              const engineer = new Engineer(name, id, email, github)
+              employees.push(engineer)
+              console.log(`You have successfully added engineer ${name}`);
+              break;
+            case 'Add intern':
+                var { name, id, email, school } = await getInternDetails();
+                const intern = new Intern(name, id, email, school)
+                employees.push(intern)
+                console.log(`You have successfully added intern ${name}`);
+              break; 
+            }
+        choice = await getChoice()
+    }
+      console.log("generating " + employees.length )
+
+      const generatedHtml = render(employees)
+      console.log(generatedHtml)
+}
+
+main();
